e748d2b0a8393c6c7f76fc0cd668bc75d71de2e7
OliverChrestensen <oliver.chrestensen@hotmail.com>
2021-02-17 00:27:58 +0100

    first

--- src/main/java/com/example/demo/Controllers/Controller.java
+++ src/main/java/com/example/demo/Controllers/Controller.java
@@ -1,4 +1,15 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.Services.ErDetFredag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@org.springframework.stereotype.Controller
 public class Controller {
+    ErDetFredag erDetFredag = new ErDetFredag();
+
+    @ResponseBody
+    @GetMapping("/Fredag")
+    public String erDetFredag(){
+    return erDetFredag.isItFriday();
+    }
 }
--- src/main/java/com/example/demo/Services/ErDetFredag.java
+++ src/main/java/com/example/demo/Services/ErDetFredag.java
@@ -1,4 +1,43 @@
 package com.example.demo.Services;
 
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+
+
 public class ErDetFredag {
+
+    LocalDate date = LocalDate.now();
+    DayOfWeek day = DayOfWeek.of(date.get(ChronoField.DAY_OF_WEEK));
+
+
+    public String isItFriday() {
+
+
+        switch (day) {
+            case FRIDAY:
+                return "wuuhuuu its friday";
+            case MONDAY:
+                return "There is a long way to friday";
+            case TUESDAY:
+                return "4 days to Friday";
+            case WEDNESDAY:
+                return "Half the week is over";
+            case THURSDAY:
+                return "You can party tommorow";
+            case SATURDAY:
+                return "Are you hungover?";
+            case SUNDAY:
+                return "The week is starting tommorow";
+        }
+
+
+        return String.valueOf(day);
+    }
 }
+
+
+
+
+
